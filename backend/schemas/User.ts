d8c47b860d69f250a9e60f25afc1d6963e89d0c9
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-07-19 21:31:08 -0500

    Access control for User

--- backend/schemas/User.ts
+++ backend/schemas/User.ts
@@ -1,10 +1,21 @@
 import { text, password, relationship } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
+import { permissions, rules } from '../access';
 
 // Named export
 export const User = list({
-  // access:
-  // ui
+  access: {
+    create: () => true,
+    read: rules.canManageUsers,
+    update: rules.canManageUsers,
+    // only people with the permission can delete themselves, not just anybody can delete themself
+    delete: permissions.canManageUsers,
+  },
+  ui: {
+    // hide the backend UI from regular users
+    hideCreate: (args) => !permissions.canManageUsers(args),
+    hideDelete: (args) => !permissions.canManageUsers(args),
+  },
   fields: {
     name: text({ isRequired: true }),
     email: text({ isRequired: true, isUnique: true }),
@@ -21,6 +32,16 @@ export const User = list({
       ref: 'Order.user',
       many: true,
     }),
+    products: relationship({
+      ref: 'Product.user',
+      many: true,
+    }),
+    role: relationship({
+      ref: 'Role.assignedTo',
+      access: {
+        create: permissions.canManageUsers,
+        update: permissions.canManageUsers,
+      },
+    }),
   },
-  // TODO add roles
 });
